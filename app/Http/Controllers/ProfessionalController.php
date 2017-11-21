65dff897aea280239ccb4c6843315adba6f0f752
Mukarram Ishaq <14besemishaq@seecs.edu.pk>
2017-11-21 21:36:14 +0500

    add index method to professional and minor changes to others

--- app/Http/Controllers/ProfessionalController.php
+++ app/Http/Controllers/ProfessionalController.php
@@ -7,4 +7,13 @@ use Illuminate\Http\Request;
 class ProfessionalController extends Controller
 {
     //
+    public function index(Request $request){
+        $user = Auth::user();
+        if($user){
+            return view('professionalInformation');
+        }
+        else{
+            return redirect()->to('login')->with('type','error')->with('msg','Session expired. Login to continue');
+        }
+    }
 }
